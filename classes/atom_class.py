e438cda68c62bbc8bbdfa6ad236239573493edca
xWhaler <camelbyte@proton.me>
2025-03-07 07:13:12 -0500

    Updated pet class project with documentation and pydoc html. The project is complete and submitted.

--- classes/atom_class.py
+++ classes/atom_class.py
@@ -1,65 +1,81 @@
 
-# The Atom class as provided by the course
-
-
-GLOBAL_NAME = "Francis"
-
-def say_hi(global_name):
-    """
-    Description: say_hi is a function defined outside of main. 
-    """
-    print(f"Hello, I am {global_name}!")
-
 
 class Atom:
 
-    global_name = "Keith"
 
-    def __init__(self, atomic_number, symbol, name, atomic_mass):
+    def __init__(self, atomic_number, symbol, name, atomic_mass, isotope=14):
         self.atomic_number = atomic_number
         self.symbol = symbol
         self.name = name
         self.atomic_mass = atomic_mass
+        self.isotope = isotope 
 
-    @staticmethod
+
     def __str__(self):
-        return f"{self.name} ({self.symbol}) - {self.atomic_number}"
+        return f"{self.name} ({self.symbol}) {self.atomic_number} - {self.atomic_mass}"
+
 
     def neutrons(self):
         number = self.atomic_mass - self.atomic_number
         print(f"{self.name} has {number} neutrons")
         return number
 
+
     def grams_to_moles(self, grams):
         moles = grams / self.atomic_mass
         print(f"{grams:.1f} g is {moles:.1f} moles of {self.symbol}")
         return moles
 
-    def say_hi_inside_a_class(self):
-        say_hi(self.global_name)
+
+def grams_to_m(atom, grams):
+    m = grams / atom.atomic_mass
+    print(f"{grams: .1f} g is {m:.1f} moles of {atom.symbol}")
+
+
+
+
 
 
 if __name__ == "__main__":
+    
     """
-    Description: My main function creates instances of Atom and calls some class methods.
+    Description: The main function creates instances of Atom and calls some class methods. Created an object 
+    or instance of the Atom class called gold and used the methods defined in the class. 
 
     """
     oxygen = Atom(8, 'O', 'Oxygen', 15.999)
     carbon = Atom(6, 'C', 'Carbon', 12.001)
 
+
     # Call class methods
     oxygen.neutrons()
     oxygen.grams_to_moles(24)
     carbon.grams_to_moles(24)
+   
+    
+    gold = Atom(24, 'Au', 'Gold', 96.96)
+    print(gold.symbol)
+    print(gold)
+    gold.neutrons()
 
-    # Call the global function from inside the class
-    oxygen.say_hi_inside_a_class()
 
-    # Print the global variable from inside the class
-    print(f"Global variable inside the class definition (class variable): {Atom.global_name}")
+    print(gold.isotope)
+    print(gold.atomic_mass)
 
-    # Print the global variable from outside the class
-    print(f"Global variable outside the class definition: {GLOBAL_NAME}")
+
+
+    
+    grams_to_m(gold, 23)
+
+
+
+
+
+
+
+
+
+
 
 
 
--- classes/atom_class_var.py
+++ classes/atom_class_var.py
@@ -8,7 +8,7 @@ class Atom:
         self.mass = mass
         self.isotope = isotope
 
-    @classmethod
+
     def neutrons(cls, atom):
         """Returns the number of neutrons the element has"""
         number = atom.isotope - atom.atomic
@@ -16,13 +16,15 @@ class Atom:
         print("Difference of atom.isotope-atom.atomic: " + str(number))
         return number
 
-    @classmethod
+    @classmethod 
     def grams_to_moles(cls, atom, grams):
         """Converts the mass of an element in grams to moles"""
         moles = grams / atom.mass
         print(f"{grams:.1f} g is {moles:.1f} moles of {atom.symbol}")
         return moles
+    
 
+    
 
 if __name__ == "__main__":
     # Create Atom objects
@@ -30,7 +32,8 @@ if __name__ == "__main__":
     carbon = Atom('C', 6, 12.001, 12)
 
     # Call class methods
-    Atom.neutrons(oxygen)
     Atom.grams_to_moles(oxygen, 24)
     Atom.grams_to_moles(carbon, 24)
-
+    
+    gold = Atom('Au', 24, 96.06, 15)
+    print(gold.symbol)
